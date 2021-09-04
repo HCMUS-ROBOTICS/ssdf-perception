3f09e31748ee6a91237850bfc83a7936a8138fa2
wan2000 <hoangquan.qti@gmail.com>
2021-09-04 22:14:52 +0700

    rename module's name for convention

--- lane_segmentation/setup.py
+++ lane_segmentation/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['lane_seg'],
+    packages=['lane_segmentation'],
     package_dir={'': 'src'},
 )
 
