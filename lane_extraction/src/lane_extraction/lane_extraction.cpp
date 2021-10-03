ea71236f81420869e677fb412d621dd5ac55ac3b
Loi Ly <vinhloiit1327@gmail.com>
2021-10-03 07:42:12 +0000

    update old code

--- lane_extraction/src/lane_extraction/lane_extraction.cpp
+++ lane_extraction/src/lane_extraction/lane_extraction.cpp
@@ -1,58 +1,49 @@
 #include "lane_extraction/lane_extraction.h"
-#include "lane_extraction/window_extractor.h"
-#include "ssdf_msgs/Lane.h"
+
 #include <opencv2/highgui.hpp>
 
-LaneExtractor::LaneExtractor()
-    : _subscriberCount{0}
-{
-    std::string lanePubTopic;
-    ROS_ASSERT(ros::param::get("~pub_lane_topic", lanePubTopic));
+#include "lane_extraction/window_extractor.h"
+#include "ssdf_msgs/Lane.h"
 
-    this->_lanePub = _nh.advertise<ssdf_msgs::Lane>(
-        lanePubTopic,
-        QUEUE_SIZE,
-        [this](const ros::SingleSubscriberPublisher &sub)
-        {
-            ROS_INFO_STREAM("New subscriber from " << sub.getSubscriberName());
-            this->_subscriberCount++;
-        },
-        [this](const ros::SingleSubscriberPublisher &sub)
-        {
-            ROS_INFO_STREAM("Disconnect from subscriber " << sub.getSubscriberName());
-            this->_subscriberCount--;
-        });
+LaneExtractor::LaneExtractor() : _subscriberCount{0} {
+  std::string lanePubTopic;
+  ROS_ASSERT(ros::param::get("~pub_lane_topic", lanePubTopic));
 
-    // TODO: switch between detector
-    detectImpl = std::make_shared<WindowExtractImpl>();
+  this->_lanePub = _nh.advertise<ssdf_msgs::Lane>(
+      lanePubTopic, QUEUE_SIZE,
+      [this](const ros::SingleSubscriberPublisher &sub) {
+        ROS_INFO_STREAM("New subscriber from " << sub.getSubscriberName());
+        this->_subscriberCount++;
+      },
+      [this](const ros::SingleSubscriberPublisher &sub) {
+        ROS_INFO_STREAM("Disconnect from subscriber " << sub.getSubscriberName());
+        this->_subscriberCount--;
+      });
+
+  // TODO: switch between detector
+  detectImpl = std::make_shared<WindowExtractImpl>();
 }
 
-LaneExtractor::~LaneExtractor()
-{
-    cv::destroyAllWindows();
-}
+LaneExtractor::~LaneExtractor() { cv::destroyAllWindows(); }
 
-void LaneExtractor::run()
-{
-    ssdf_msgs::Lane msg;
-    std::vector<double> leftParams;
-    std::vector<double> rightParams;
-    ros::Rate publishRate{static_cast<double>(PUBLISH_RATE)};
+void LaneExtractor::run() {
+  ssdf_msgs::Lane msg;
+  std::vector<double> leftParams;
+  std::vector<double> rightParams;
+  ros::Rate publishRate{static_cast<double>(PUBLISH_RATE)};
 
-    while (_nh.ok())
-    {
-        ros::spinOnce();
-        detectImpl->update();
+  while (_nh.ok()) {
+    ros::spinOnce();
+    detectImpl->update();
 
-        if (this->_subscriberCount > 0)
-        {
-            const LaneParams &leftParams = detectImpl->getLeftParams();
-            const LaneParams &rightParams = detectImpl->getRightParams();
-            msg.left = leftParams;
-            msg.right = rightParams;
+    if (this->_subscriberCount > 0) {
+      const LaneParams &leftParams = detectImpl->getLeftParams();
+      const LaneParams &rightParams = detectImpl->getRightParams();
+      msg.left = leftParams;
+      msg.right = rightParams;
 
-            this->_lanePub.publish(msg);
-        }
-        publishRate.sleep();
+      this->_lanePub.publish(msg);
     }
+    publishRate.sleep();
+  }
 }
--- lane_segmentation/bin/node.py
+++ lane_segmentation/bin/node.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 import rospy
+
 from lane_segmentation.segmentation import Segmentation
 from lane_segmentation.unet_model import UnetModel
+
 
 def main():
     rospy.init_node('lane_segmentation_node')
@@ -14,5 +16,6 @@ def main():
 
     segmentation.run_loop()
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
